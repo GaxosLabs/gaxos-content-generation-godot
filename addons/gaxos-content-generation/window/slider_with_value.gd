d881142871222a4653035a51d6074813775a8d87
israelillan <40181073+israelillan@users.noreply.github.com>
2024-11-15 09:31:56 -0500

    fixes from juan

--- addons/gaxos-content-generation/window/slider_with_value.gd
+++ addons/gaxos-content-generation/window/slider_with_value.gd
@@ -3,7 +3,13 @@ extends HBoxContainer
 	
 func _ready() -> void:
 	$HSlider.value_changed.connect(_refresh_text)
+	$TextEdit.text_changed.connect(_text_changed)
 	_refresh_text($HSlider.value)
 
+func _text_changed(v: String) -> void:
+	print(v)
+	
+@export var numberOfDecimals: int = 0
+
 func _refresh_text(v: float) -> void:
-	$TextEdit.text = "%f" % v
+	$TextEdit.text = "%.*f" % [numberOfDecimals, v]
